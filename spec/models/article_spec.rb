0fe7f36e80af2680708388b32b3415c1990fecb7
Rithreaksa <wonderfulreaksa@gmail.com>
2021-01-17 15:06:32 +0100

    Writes test to check journalist can create premium articles

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,6 +3,8 @@ RSpec.describe Article, type: :model do
     it { is_expected.to have_db_column :title }
     it { is_expected.to have_db_column :sub_title }
     it { is_expected.to have_db_column :content}
+    it { is_expected.to have_db_column :premium}
+
   end
   describe 'Validations' do
     it { is_expected.to validate_presence_of :title }
